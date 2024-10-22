c8018cfab5e8d05f3face66281c2d2e5f682a616
Sachin-nsk <sachinkumarnsk20090@gmail.com>
2024-10-22 20:51:45 +0530

    Initial Navbar Template

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'  // Add this line
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
